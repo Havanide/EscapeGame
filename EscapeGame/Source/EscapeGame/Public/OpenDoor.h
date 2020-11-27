9708be8b08e03f6237bbdca3ead3358f1f1f17a0
LexLichkov <alexlichkovakha@gmail.com>
2020-11-27 17:48:56 +0300

    Create TriggerVolume and UPROPERTY

--- EscapeGame/Source/EscapeGame/Public/OpenDoor.h
+++ EscapeGame/Source/EscapeGame/Public/OpenDoor.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/TriggerVolume.h"
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
 
@@ -24,5 +25,11 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
+	UPROPERTY(VisibleAnywhere)
+	float openAngle = 90.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
 		
 };
